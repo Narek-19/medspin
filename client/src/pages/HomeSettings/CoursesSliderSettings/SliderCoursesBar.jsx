2b495d0f5dd353b0c339fef78f065a4faed2a351
Narek-19 <narekchakhoyan97@gmail.com>
2022-04-15 09:12:37 +0400

    changed-react-file-for sliders

--- client/src/pages/HomeSettings/CoursesSliderSettings/SliderCoursesBar.jsx
+++ client/src/pages/HomeSettings/CoursesSliderSettings/SliderCoursesBar.jsx
@@ -1,10 +1,26 @@
+import { connect } from "react-redux";
 import React from "react";
+import {getSlider} from "../../../redux/actions";
 import * as styles from './style.module.css';
 
-export const SliderCoursesBar =()=>{
+const SliderCoursesBar =(props)=>{
+    
+    const arraySlide = [
+        {title:"Title1",img:"https://klass.novaclearning.com/theme/images/default.jpg",des:"message1"},
+        {title:"Title2",img:"https://klass.novaclearning.com/theme/images/default.jpg",des:"message2"},
+        {title:"Title3",img:"https://klass.novaclearning.com/theme/images/default.jpg",des:"message3"}
+    ]
+
+    const handleChange =()=>{
+        props.getSlider(arraySlide);
+    }
+
     return(
         <div className = {styles.SliderCoursesBar}>
-
+            <button onClick = {handleChange}>
+                Click
+            </button>
         </div>
     )
 }
+export default connect(null,getSlider)(SliderCoursesBar);
